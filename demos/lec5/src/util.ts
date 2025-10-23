061fa25a7821cdc4f36467669b6988fb2f6ed44e
Jacqueline <jacqueline.cai2004@gmail.com>
2025-10-23 15:17:15 -0400

    finished lec5 demo

--- demos/lec5/src/util.ts
+++ demos/lec5/src/util.ts
@@ -33,4 +33,74 @@ interface ApiMeeting {
 export const fetchCourseDetails = async (
   subject: string,
   catalogNbr: number
-): Promise<Partial<Course>> => {};
+): Promise<Partial<Course>> => {
+  try {
+    const response = await fetch(
+      `https://classes.cornell.edu/api/2.0/search/classes.json?roster=FA25&subject=${subject}`,
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+        },
+      }
+    );
+
+    // rejected case
+    if (!response.ok) {
+      throw new Error(`API request failed with status ${response.status}`);
+    }
+
+    // successful case
+    // retrieving the data
+    const data = (await response.json()) as {
+      data: {
+        classes: ApiCourseData[];
+      };
+    };
+
+    // find specific course
+    const specificCourseData = await data.data.classes.find(
+      (course: ApiCourseData) => Number(course.catalogNbr) === catalogNbr
+    );
+
+    if (!specificCourseData) {
+      console.warn(
+        `Course ${subject} ${catalogNbr} not found in Cornell Course Roster API`
+      );
+      return {};
+    }
+
+    const details: Partial<Course> = {};
+
+    if (specificCourseData.description) {
+      details.description = specificCourseData.description;
+    }
+
+    // TODO: catalogWhenOffered, credits, instructors
+    if (specificCourseData.catalogWhenOffered) {
+      details.whenOffered = specificCourseData.catalogWhenOffered;
+    }
+
+    if (
+      specificCourseData.enrollGroups &&
+      specificCourseData.enrollGroups.length > 0
+    ) {
+      details.credits = specificCourseData.enrollGroups[0].unitsMinimum;
+    }
+
+    if (
+      specificCourseData.enrollGroups &&
+      specificCourseData.enrollGroups.length > 0 &&
+      specificCourseData.enrollGroups[0].classSections.length > 0 &&
+      specificCourseData.enrollGroups[0].classSections[0].meetings.length > 0
+    ) {
+      details.instructors =
+        specificCourseData.enrollGroups[0].classSections[0].meetings[0].instructors;
+    }
+
+    return details;
+  } catch (err) {
+    console.error("Error fetching course details:", err);
+    return {};
+  }
+};
