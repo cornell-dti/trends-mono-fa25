432c19dedfa6c9cffce3f48df5e53e353c943786
Simon Ilincev <trilogicworlds@gmail.com>
2025-11-18 15:31:52 -0500

    fix: update environment variable for API base URL in client configuration

--- demos/lec9/client/src/util.ts
+++ demos/lec9/client/src/util.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:8080/api";
+const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:8080/api";
 
 /**
  * Makes an array with the integers from 0 (inclusive) to n (exclusive).
